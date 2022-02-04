866f76560383afe2fa312246665928774a57bab3
Elane Ho <elaine_ho@ucsb.edu>
2022-02-04 09:45:59 -0800

    finished flix app

---
+++ flix/MovieCell.swift
@@ -0,0 +1,32 @@
+//
+//  MovieCell.swift
+//  flix
+//
+//  Created by elaine ho on 2/3/22.
+//  Copyright © 2022 elaine ho. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
