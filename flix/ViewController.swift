aaaf32e4cc9f03614a0a9dc93514908f3b17cc0d
Elane Ho <elaine_ho@ucsb.edu>
2022-02-03 19:59:34 -0800

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by elaine ho on 2/3/22.
+//  Copyright © 2022 elaine ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
